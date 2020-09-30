747f6788cbca3987ab49299ef981cd54957a8bd9
Jeff <Jeff.Crainshaw@gmail.com>
2020-09-30 12:02:54 -0500

    guess this is where im doing expirements now

---
+++ javascript.js
@@ -0,0 +1,26 @@
+function myFunction() {
+    console.log("good so far");
+
+    let x = document.getElementById("thing");
+}
+
+function myFunction2() {
+    var x = 1;
+    let y = 2;
+    let z = 3;
+
+    while (x < 10) {
+        console.log(x);
+        x++;
+    }
+
+    while (y < 10) {
+        console.log(y);
+        y++;
+    }
+
+    while (z < 10) {
+        console.log(z);
+        z++;
+    }
+}
